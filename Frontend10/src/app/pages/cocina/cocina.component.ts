0659be105899adcbf5e4988d74254eb580199d1f
yefringeovany <yperezm18@miumg.edu.gt>
2024-09-09 10:34:55 -0600

    Agregando diseño en cocina

--- Frontend10/src/app/pages/cocina/cocina.component.ts
+++ Frontend10/src/app/pages/cocina/cocina.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CocinaService } from './cocina.service';
 import { OrdenService } from '../ordenes/orden.service';
 import { API_BASE_URL } from 'src/api.config'; // Asegúrate de importar la URL base de la API
+import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-cocina',
@@ -13,7 +15,8 @@ export class CocinaComponent implements OnInit {
 
   constructor(
     private cocinaService: CocinaService,
-    private ordenService: OrdenService
+    private ordenService: OrdenService,
+    private snackBar: MatSnackBar
   ) { }
 
   ngOnInit(): void {
@@ -30,20 +33,36 @@ export class CocinaComponent implements OnInit {
       }
     );
   }
-
   entregarOrden(id: number) {
     const idNumero = Number(id);
     this.ordenService.actualizarOrden(idNumero, 'TERMINADO').subscribe(
       () => {
         this.listadoOrdenesPendientes();
+        this.showSuccessMessage('Orden entregada con éxito'); // Mostrar notificación de éxito
       },
       (error) => {
         console.error('Error al entregar la orden:', error);
+        this.showErrorMessage('Error al entregar la orden'); // Mostrar notificación de error
       }
     );
-
-
   }
+
+  private showSuccessMessage(message: string) {
+    this.snackBar.open(message, 'Cerrar', {
+      duration: 5000,
+      verticalPosition: 'top',
+      panelClass: ['success-snackbar']
+    });
+  }
+
+  private showErrorMessage(message: string) {
+    this.snackBar.open(message, 'Cerrar', {
+      duration: 5000,
+      verticalPosition: 'top',
+      panelClass: ['error-snackbar']
+    });
+  }
+
   getImageUrl(imageName: string): string {
     return `http://localhost:5000/uploads/${imageName}`; // Ajusta esta URL según la ubicación de tus imágenes
   }
