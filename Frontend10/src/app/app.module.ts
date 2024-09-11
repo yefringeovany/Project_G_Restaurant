1aeedbf74943c15f7b3c4f061048cf0ea808af71
yefringeovany <yperezm18@miumg.edu.gt>
2024-09-11 00:59:06 -0600

    haciendo cambios en el frontend

--- Frontend10/src/app/app.module.ts
+++ Frontend10/src/app/app.module.ts
@@ -1,29 +1,33 @@
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
-import {RouterModule} from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon'; // Si usas iconos
+import { MatButtonModule } from '@angular/material/button'; // Si usas botones
+import { MatListModule } from '@angular/material/list'; // Si usas listas
 
 
-import {AppComponent} from './app.component';
-import {AdminLayoutComponent} from './layouts/admin-layout/admin-layout.component';
-import {AuthLayoutComponent} from './layouts/auth-layout/auth-layout.component';
 
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
-import {AppRoutingModule} from './app.routing';
-import {ComponentsModule} from './components/components.module';
-import {NgOptimizedImage} from "@angular/common";
+import { AppComponent } from './app.component';
+import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { InformacionComponent } from './pages/informacion/informacion.component'; // Ajusta la ruta según la ubicación del componente
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app.routing';
+import { ComponentsModule } from './components/components.module';
+import { NgOptimizedImage } from "@angular/common";
 import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 import { CategoriasComponent } from './pages/categorias/categorias.component';
 import { MenusComponent } from './pages/menus/menus.component';
 import { OrdenesComponent } from './pages/ordenes/ordenes.component';
 import { PosComponent } from './pages/pos/pos.component';
 import { CocinaComponent } from './pages/cocina/cocina.component';
-import { InformacionComponent } from './pages/informacion/informacion.component';
 // import { ReportesComponent } from './pages/reportes/reportes.component';
-
 
 @NgModule({
   imports: [
@@ -35,7 +39,11 @@ import { InformacionComponent } from './pages/informacion/informacion.component'
     RouterModule,
     AppRoutingModule,
     NgOptimizedImage,
-    MatSnackBarModule
+    MatSnackBarModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule
   ],
   declarations: [
     AppComponent,
@@ -53,5 +61,4 @@ import { InformacionComponent } from './pages/informacion/informacion.component'
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule { }
--- Frontend10/src/app/pages/informacion/informacion.component.ts
+++ Frontend10/src/app/pages/informacion/informacion.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 
+interface RestaurantInfo {
+  name: string;
+  description: string;
+  locations: { address: string; phone: string }[];
+  promotions: { title: string; description: string }[];
+  website: string;
+  socialMedia: { facebook: string; instagram: string; twitter: string };
+}
+
 @Component({
   selector: 'app-informacion',
   templateUrl: './informacion.component.html',
   styleUrls: ['./informacion.component.scss']
 })
 export class InformacionComponent implements OnInit {
+  restaurantInfo: RestaurantInfo = {
+    name: 'La Delicia',
+    description: 'La Delicia es un restaurante familiar que ofrece la mejor comida tradicional con un toque moderno.',
+    locations: [
+      { address: 'Calle Principal 123, Ciudad Ejemplo, País', phone: '+1 234 567 8900' },
+      { address: 'Avenida Secundaria 456, Ciudad Ejemplo, País', phone: '+1 098 765 4321' }
+    ],
+    promotions: [
+      { title: 'Martes de Tacos', description: '2x1 en todos nuestros tacos cada martes' },
+      { title: 'Happy Hour', description: 'Bebidas al 50% de descuento de 5pm a 7pm' }
+    ],
+    website: 'www.ladelicia.com',
+    socialMedia: {
+      facebook: 'facebook.com/ladelicia',
+      instagram: 'instagram.com/ladelicia',
+      twitter: 'twitter.com/ladelicia'
+    }
+  };
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
 }
