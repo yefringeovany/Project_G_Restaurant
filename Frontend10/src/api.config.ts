82ffce5cd988c1a24b9c09d8970af1a506f23044
yefringeovany <yperezm18@miumg.edu.gt>
2024-10-02 15:47:24 -0600

    prueba despliegue 11

--- Frontend10/src/api.config.ts
+++ Frontend10/src/api.config.ts
@@ -1,1 +1,3 @@
-export const API_BASE_URL = 'http://localhost:5000';
+// export const API_BASE_URL = 'http://localhost:5000';
+
+export const API_BASE_URL = 'https://project-g-restaurant.onrender.com';
